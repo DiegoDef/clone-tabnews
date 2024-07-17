d064353a766dbea38d69704085fd2bd008f16742
Diego Rossi Corrêa <diegorossicorrea2000@gmail.com>
2024-07-17 09:25:15 -0300

    lint fix

--- infra/database.js
+++ infra/database.js
@@ -1,21 +1,20 @@
-import { Client } from 'pg'
-
+import { Client } from "pg";
 
 async function query(query) {
-    const client = new Client({
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
-        user: process.env.POSTGRES_USER,
-        database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD
-    });
-    await client.connect()
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
 
-    const res = await client.query(query)
-    client.end();
-    return res;
+  const res = await client.query(query);
+  client.end();
+  return res;
 }
 
 export default {
-    query: query
-}
+  query: query,
+};
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,12 +1,14 @@
 test("GET to api/v1/status should return 200, updated_at, postgres_version and connections", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   const responseBody = await response.json();
-  console.log(responseBody)
+  console.log(responseBody);
 
   expect(response.status).toBe(200);
-  expect(responseBody.updated_at).toBeDefined()
-  expect(new Date(responseBody.updated_at).toISOString()).toEqual(responseBody.updated_at)
-  expect(responseBody.dependencies.database.postgres_version).toEqual('16.0');
-  expect(responseBody.dependencies.database.max_connections).toEqual(100)
-  expect(responseBody.dependencies.database.used_connections).toEqual(1)
+  expect(responseBody.updated_at).toBeDefined();
+  expect(new Date(responseBody.updated_at).toISOString()).toEqual(
+    responseBody.updated_at,
+  );
+  expect(responseBody.dependencies.database.postgres_version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.used_connections).toEqual(1);
 });
