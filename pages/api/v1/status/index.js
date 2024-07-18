22c40f989a6beba4c0c68c5e127614aec2558fbc
Diego Rossi Corrêa <diegorossicorrea2000@gmail.com>
2024-07-18 11:57:44 -0300

    add parameterized query

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,17 +2,21 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
-  const query = await database.query(
-    "select current_setting('server_version') AS postgres_version,  count(psa.*)::int as used_connections, (SELECT current_setting('max_connections')::int) AS max_connections FROM pg_stat_activity psa WHERE state = 'active';",
-  );
+  const databaseName = process.env.POSTGRES_DB;
+  const query = {
+    text: "select current_setting('server_version') AS postgres_version,  count(psa.*)::int as used_connections, (SELECT current_setting('max_connections')::int) AS max_connections FROM pg_stat_activity psa WHERE state = 'active' and datname = $1;",
+    values: [databaseName],
+  };
+
+  const res = await database.query(query);
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
-        postgres_version: query.rows[0].postgres_version,
-        max_connections: query.rows[0].max_connections,
-        used_connections: query.rows[0].used_connections,
+        postgres_version: res.rows[0].postgres_version,
+        max_connections: res.rows[0].max_connections,
+        used_connections: res.rows[0].used_connections,
       },
     },
   });
